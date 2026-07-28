7ec9f10f3cca78f0dd75112c27227172f3ca7358
VSantos77 <santiago.villaverde07@gmail.com>
2026-07-28 18:29:29 -0300

    create singular test with severity = warn for discounts without rate and no interest instll qty

---
+++ discount_tracker_dbt/tests/int_joined_discounts_rate_and_installments_both_zero.sql
@@ -0,0 +1,14 @@
+{{
+    config(
+        severity = 'warn'
+    )
+}}
+
+SELECT
+    discount_id,
+    source_id,
+    issuer_name
+FROM {{ ref('int_joined_deduped_and_normalized_discounts') }}
+WHERE 
+    discount_rate = 0
+    AND discount_no_interest_installment_qty = 0
