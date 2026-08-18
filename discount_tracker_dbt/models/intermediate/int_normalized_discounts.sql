54afe713815a97a313a0ec10098e16ecbd4a9f20
VSantos77 <santiago.villaverde07@gmail.com>
2026-08-18 18:37:00 -0300

    adjust logic for merchant category mapping

--- discount_tracker_dbt/models/intermediate/int_normalized_discounts.sql
+++ discount_tracker_dbt/models/intermediate/int_normalized_discounts.sql
@@ -7,14 +7,19 @@ category_override as (select * from {{ ref('merchant_category_override') }}),
 category_mapping as (select * from {{ ref('merchant_category_mapping') }})
 
 select
-    s.*,
+    s.* except(
+        merchant_category_name,
+        merchant_name
+    ),
     coalesce(
         co.normalized_category,
-        cm.clean_category,
+        cm.parsed_merchant_category_name,
         'Sin categorizar'
-    ) as merchant_category_clean
+    ) as merchant_category_clean,
+    initcap(s.merchant_name) as merchant_name
 from source as s
 left join category_override as co
     on lower(s.merchant_name) = lower(co.merchant_name)
 left join category_mapping as cm
-    on lower(s.merchant_category_name) = lower(cm.raw_category)
+    on lower(cm.issuer) = lower(s.issuer_name)
+    and lower(s.merchant_category_name) = lower(cm.raw_merchant_category_name)
