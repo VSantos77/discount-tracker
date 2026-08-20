cf3345a1b24fe7fc3a40214311ebf3dc37042464
Santiago Villaverde <62660725+VSantos77@users.noreply.github.com>
2026-08-20 18:59:03 -0300

    Feature/ci add deploy step for dbt (#6)
    * Add IAM Policy for CI Service Account to act as dbt job
    
    * Add deploy job to dbt CI workflow

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -1,15 +1,21 @@
+### Common
+
+# Create Service Account for cloud run job updates
 resource "google_service_account" "github-actions-deployer-sa" {
     account_id = "github-actions-deploy"
     display_name = "Github Actions Deploy Service Account"
 }
 
-### Scrapy
+# Grant Cloud Run developer role to SA to edit Cloud Run Jobs
 resource "google_project_iam_member" "github-actions-deploy-update-cloud-run-job" {
   project = var.gcp_project_id
   role    = "roles/run.developer"
   member  = "serviceAccount:${google_service_account.github-actions-deployer-sa.email}"
 }
 
+### Scrapy
+
+# Allow CI SA to act as Scrapy Cloud Run Job (needed for edits)
 resource "google_service_account_iam_member" "github-actions-deployer-run-as-scrapy-job" {
   service_account_id = google_service_account.scraper_sa.name
   role               = "roles/iam.serviceAccountUser"
@@ -18,13 +24,21 @@ resource "google_service_account_iam_member" "github-actions-deployer-run-as-scr
 
 ### DBT
 
-# Create SA for github actions runner
+# Allow CI SA to act as DBT Cloud Run Job (needed for edits)
+resource "google_service_account_iam_member" "github-actions-deployer-run-as-dbt-job" {
+  service_account_id = google_service_account.dbt_sa.name
+  role               = "roles/iam.serviceAccountUser"
+  member             = "serviceAccount:${google_service_account.github-actions-deployer-sa.email}"
+}
+
+# Create SA for DBT CI Target
 resource "google_service_account" "dbt_ci_sa" {
     account_id = "dbt-ci-sa"
     display_name = "Github Actions CI DBT Service Account"
 }
 
 ## ROLE GRANTS
+
 # Run queries
 resource "google_project_iam_member" "dbt_ci_sa_bq_job_user" {
   project = var.gcp_project_id
