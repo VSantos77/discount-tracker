a3db9b030b635685556cb834a2e697b5f269a58e
Santiago Villaverde <62660725+VSantos77@users.noreply.github.com>
2026-08-20 14:00:15 -0300

    Feature/add dbt test to ci workflow (#5)
    * provision gcp resources for CI dbt test
    
    * create dedicated dbt profile for CI
    
    * create dedicated github actions workflow to run CI test for dbt

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -3,6 +3,7 @@ resource "google_service_account" "github-actions-deployer-sa" {
     display_name = "Github Actions Deploy Service Account"
 }
 
+### Scrapy
 resource "google_project_iam_member" "github-actions-deploy-update-cloud-run-job" {
   project = var.gcp_project_id
   role    = "roles/run.developer"
@@ -13,4 +14,35 @@ resource "google_service_account_iam_member" "github-actions-deployer-run-as-scr
   service_account_id = google_service_account.scraper_sa.name
   role               = "roles/iam.serviceAccountUser"
   member             = "serviceAccount:${google_service_account.github-actions-deployer-sa.email}"
+}
+
+### DBT
+
+# Create SA for github actions runner
+resource "google_service_account" "dbt_ci_sa" {
+    account_id = "dbt-ci-sa"
+    display_name = "Github Actions CI DBT Service Account"
+}
+
+## ROLE GRANTS
+# Run queries
+resource "google_project_iam_member" "dbt_ci_sa_bq_job_user" {
+  project = var.gcp_project_id
+  role    = "roles/bigquery.jobUser"
+  member  = "serviceAccount:${google_service_account.dbt_ci_sa.email}"
+}
+
+# Read and write tables/views (viewer only to raw + editor to dev datasets)
+
+resource "google_project_iam_member" "dbt_ci_bq_editor" {
+  project    = var.gcp_project_id
+  role       = "roles/bigquery.dataEditor"
+  member     = "serviceAccount:${google_service_account.dbt_ci_sa.email}"
+}
+
+# View storage objects and metadata (access external table)
+resource "google_project_iam_member" "dbt_ci_sa_storage_object_viewer" {
+  project = var.gcp_project_id
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${google_service_account.dbt_ci_sa.email}"
 }
